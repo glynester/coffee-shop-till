08d6dd0c20281b42af4aa6858a437d1a605909cc
glynester <glynester@hotmail.com>
2016-12-01 19:33:14 +0000

    Can read from json file and assign values to variables

--- lib/app.rb
+++ lib/app.rb
@@ -3,41 +3,16 @@ require 'json'
 
 class Shop
 
-  attr_reader :shopname
+  attr_reader :shopname, :address, :phone, :prices
 
   def initialize(details_file)
-    # @shopname
     file = File.read(details_file)
     data_hash = JSON.parse(file)
-    puts data_hash
+    @shopname = data_hash[0]["shopName"]  #Array of hash
+    @address = data_hash[0]["address"]    #Array of hash
+    @phone = data_hash[0]["phone"]        #Array of hash
+    @prices = data_hash[0]["prices"]      #Array of hash
+    puts @prices
   end
 
 end
-
-
-[
-  {
-    "shopName": "The Coffee Connection",
-    "address": "123 Lakeside Way",
-    "phone": "16503600708",
-    "prices": [
-      {
-        "Cafe Latte": 4.75,
-        "Flat White": 4.75,
-        "Cappucino": 3.85,
-        "Single Espresso": 2.05,
-        "Double Espresso": 3.75,
-        "Americano": 3.75,
-        "Cortado": 4.55,
-        "Tea": 3.65,
-        "Choc Mudcake": 6.40,
-        "Choc Mousse": 8.20,
-        "Affogato": 14.80,
-        "Tiramisu": 11.40,
-        "Blueberry Muffin": 4.05,
-        "Chocolate Chip Muffin": 4.05,
-        "Muffin Of The Day": 4.55
-      }
-    ]
-  }
-]

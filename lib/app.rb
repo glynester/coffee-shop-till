0701077ba743c6680ceabfa2c578feae787d2069
glynester <glynester@hotmail.com>
2016-12-01 20:08:27 +0000

    Interim commit

--- lib/app.rb
+++ lib/app.rb
@@ -1,6 +1,5 @@
 require 'json'
 
-
 class Shop
 
   attr_reader :shopname, :address, :phone, :prices
@@ -12,7 +11,28 @@ class Shop
     @address = data_hash[0]["address"]    #Array of hash
     @phone = data_hash[0]["phone"]        #Array of hash
     @prices = data_hash[0]["prices"]      #Array of hash
-    puts @prices
+    # @discount_item = ""
+    # @discount_period = [@start_date, @end_date]
+    # @discount_table = [@discount_item, @discount_period]
+    # @spend_amt_before_discount
+  end
+
+  def general_discount(spend_amt)
+    @spend_amt_before_discount = spend_amt
+  end
+
+  def make_discount_table(discount_item, start_date = "N/A", end_date = "N/A")
+    @discount_item = discount_item
+    # start_date != "N/A"
+    @discount_period = [start_date, end_date]
+    @discount_table = [@discount_item, @discount_period]
+  end
+
+end
+
+class Till
+  def initialize
+
   end
 
 end
